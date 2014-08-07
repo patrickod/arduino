d2a977e6b56f8b99ca5caa8d2ebcb9db736a68af
Patrick O'Doherty <p@trickod.com>
2014-08-06 22:15:06 -0700

    Reordered function definitions to avoid warnings.
    Added Makefile for working outside of the Arduino IDE.

--- rgb_fade/rgb_fade.ino
+++ rgb_fade/rgb_fade.ino
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
 const int MAX_BRIGHTNESS = 64;
 int bluePin = 11;
 int redPin = 10;
 int greenPin = 9;
+
+void setColorRgb(unsigned int red, unsigned int green, unsigned int blue) {
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+}
 
 void setup() {
   pinMode(redPin, OUTPUT);
@@ -15,21 +23,15 @@ void loop() {
   rgbColor[0] = MAX_BRIGHTNESS;
   rgbColor[1] = 0;
   rgbColor[2] = 0;
-  
+
   for(int decColor = 0; decColor < 3; decColor += 1) {
-    int intColor = decColor == 2 ? 0 : decColor + 1;
+    int incColor = decColor == 2 ? 0 : decColor + 1;
     for(int i = 0; i < MAX_BRIGHTNESS; i += 1) {
       rgbColor[decColor] -= 1;
-      rgbColor[intColor] += 1;
+      rgbColor[incColor] += 1;
       setColorRgb(rgbColor[0], rgbColor[1], rgbColor[2]);
-      delay(10);
+      delay(60);
     }
   }
-}
-
-void setColorRgb(unsigned int red, unsigned int green, unsigned int blue) {
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);
 }
 
